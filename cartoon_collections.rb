f2598d2c64f9dca7d1fb76276b2a9bcd7d693acc
Henry Velasquez <hvelas93@gmail.com>
2021-02-15 16:32:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index { |item, index|
-    puts "/#{index + 1}.*{item}/" }
+    puts "/#{index + 1}.*#{item}/" }
 
 end
